a293a62820b02dece5af96459746d3e8514de907
BenjaminCloverWilson <89162179+BenjaminCloverWilson@users.noreply.github.com>
2021-10-13 11:26:25 -0600

    Update zombie.h
    Comments

--- zombie.h
+++ zombie.h
@@ -1,10 +1,10 @@
-/**                                                                                        
+/**
  * @file zombie.h
- * @brief header file for lab6.c and zombie.c 
+ * @brief header file for lab6.c and zombie.c
  * @author Benjamin Wilson
  * @date Fall 2021
- * @bug None yet
- * @todo Everything
+ * @bug Probably
+ * @todo none
  */
 
 #ifndef ZOMBIE_H
@@ -13,9 +13,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Array sizes for user input and the zombie log itself */
 #define SIZE 16
 #define LOG 5
 
+/* Holds stats about zombie */
 struct zombie_t {
     enum DAY {MON = 1, TUE, WED, THU, FRI, SAT, SUN} day;
 
@@ -27,11 +29,12 @@ struct zombie_t {
     char dead;
 };
 
+/* Function prototypes that are defined in zombie.c */
 struct zombie_t get_zombie();
 int input_toes();
 float input_blood();
 int input_day();
 int input_time(char unit, char in_time[]);
-void print_zombies(struct zombie_t zombie[], int count);
+void print_zombies(struct zombie_t zombie[], int total_count);
 
 #endif
