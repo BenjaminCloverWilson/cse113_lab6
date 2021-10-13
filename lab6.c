f34f85fd457ef67049a56a440f6b3cc634a2c769
BenjaminCloverWilson <89162179+BenjaminCloverWilson@users.noreply.github.com>
2021-10-13 11:24:19 -0600

    Update lab6.c
    Comments

--- lab6.c
+++ lab6.c
@@ -1,20 +1,34 @@
+/**
+ * @file lab6.c
+ * @brief Menu and calls for zombie program. Logs up-to 5 zombies
+ * @author Benjamin Wilson
+ * @date Fall 2021
+ * @bug Probably
+ * @todo none
+ */
+
 #include "zombie.h"
 
 int main()
 {
+    /* Holds zombie stats in log */
     struct zombie_t zombie;
     struct zombie_t log[LOG];
 
+    /* User-input arrays and integers */
     char in[SIZE];
     int in_num;
 
+    /* Loop and log counting variables */
     int loop = 1;
     int log_loop = 1;
     int count = 0;
+    int total_count = 0;
 
     printf("\n---Welcome to Benjamin Wilson's Zombie Log---\n");
     printf("---Serving the surviving members of humanity since 2021---\n");
 
+    /* Checks that valid user-input of menu options */
     while(loop == 1)
     {
     
@@ -22,14 +36,19 @@ int main()
     printf("2) Display zombie information\n");
     printf("3) Return to fighting zombies (exit)\n");
     printf(">> ");
+    /* Takes user-input and converts to integer for menu */
     fgets(in, SIZE, stdin);
     in_num = atoi(in);
 
+    /* Menu option calls and printf() */
     switch(in_num)
     {
+        /* Input zombie and place it in log */
         case 1:
+            /* Tells user that max log number (5) is reached */
             if(count == LOG)
             {
+                /* Checks for valid user-input */
                 do
                 {
                     /* printf() split for display purposes */
@@ -40,8 +59,10 @@ int main()
                     printf("are overwritten.\n");
                     printf("Continue? Y or N\n");
                     printf(">> ");
+                    /* User-input */
                     fgets(in, SIZE, stdin);
 
+                    /* Take input, start count over, overwrite log one */
                     if((in[0] == 'Y' && in[1] == '\n') || (in[0] == 'y' && in[1] == '\n'))
                     {
                         zombie = get_zombie(zombie);
@@ -50,30 +71,44 @@ int main()
                         count++;
                         log_loop = 2;
 
+                    /* Do nothing but exit user-input loop for yes or no */
                     } else if((in[0] == 'N' && in[1] == '\n') || (in[0] == 'n' && in[1] == '\n'))
                     {
                         log_loop = 2;
-                        break;
 
-                } else
-                {
-                    printf("\nInvalid user input, please try again!\n");
-                }
+                    } else
+                    {
+                        printf("\nInvalid user input, please try again!\n");
+                    }
 
                 } while(log_loop == 1);
 
+            /* get user-inputted zombie stats, and place in log based on count */
             } else if(count < LOG)
             {
                 zombie = get_zombie(zombie);
                 log[count] = zombie;
                 count++;
-                break;
+                
+                /**
+                 * How many zombies are in log at any time. Does not start
+                 * over if overwritting of previous logs begins. Creates
+                 * circular buffer
+                 */
+                if(total_count != 5)
+                {
+                    total_count++;
+                }
             }
 
+            break;
+
+        /* Print log of zombie stats */
         case 2:
-            print_zombies(log, count);
+            print_zombies(log, total_count);
             break;
         
+        /* Terminate and quit program */
         case 3:
             /* printf() split for display purposes */
             printf("\nStay alert. Watch out for zombies! GOODBYE and ");
@@ -82,6 +117,7 @@ int main()
 
             return 0;
         
+        /* Invalid menu input */
         default:
             /* printf() split for display purposes */
             printf("\nInvalid user input. Time is of the essence, pay");
